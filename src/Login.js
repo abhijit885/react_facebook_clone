eeec59bbb55508a11fe8c1964f81b2b2bb87bd72
abhijit <abhijitsaha885@gmail.com>
2022-01-13 15:14:31 +0530

    #code update

--- src/Login.js
+++ src/Login.js
@@ -1,39 +1,39 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 import './Login.css'
-import { auth, provider} from './firebase'
+import { auth, provider } from './firebase'
 import { useStateValue } from './StateProvider'
 import { actionTypes } from './reducer'
 
 function Login() {
-    const [state , dispatch] = useStateValue()
+    const [state, dispatch] = useStateValue()
 
     const signIn = () => {
         auth
-        .signInWithPopup(provider)
-        .then((result) => {
+            .signInWithPopup(provider)
+            .then((result) => {
 
-            dispatch({
-                type: actionTypes.SET_USER,
-                user: result.user,
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                })
+                console.log(result.user)
             })
-            console.log(result.user)
-        })
-        .catch((error) => alert(error.message))
+            .catch((error) => alert(error.message))
     }
     return (
         <div className="login">
             <div className="login__logo">
                 <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
-                alt=""/>
-                <img 
-                src="https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-1-1.png"
-                alt=""
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+                    alt="" />
+                <img
+                    src="https://logodownload.org/wp-content/uploads/2014/09/facebook-logo-1-1.png"
+                    alt=""
                 />
             </div>
             <Button type="submit" onClick={signIn}>
-                Sign In
+                Sign In 13-01-22
             </Button>
         </div>
     )
